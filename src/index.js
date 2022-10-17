fc64c3302c2d0f13a17aec5ee72740883e1d8d55
Kettengeschwindigkeit <666damned999@mail.ru>
2022-10-17 13:11:29 +0300

    Redux. Part I. Higher-order functions

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const App = (params) => {
+  const arr = ["some", "new", "data"]
 
-  function someFn() {
-    return function() {
-      return "app"
-    }
-  }
+  function formatArray(el) {return el + " some"}
 
-  const fn = someFn()
+  setTimeout(() => {
 
-  return <h1>{fn()}</h1>
+  }, timeout)
+
+  // arr.map((el) => el + " some")
+
+  return <h1>{arr.map(formatArray)}</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -27,75 +28,18 @@ root.render(
 // import ReactDOM from 'react-dom/client'
 
 // const App = (params) => {
-//   function fn() {
-//     return "app"
+
+//   function someFn() {
+//     return function() {
+//       return "app"
+//     }
 //   }
-//   function someFn(func) {
-//     return func
-//   }
-//   return <h1>{someFn(fn())}</h1>
-// }
 
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
-
-// ====================================================================================================================================================================
-
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-
-// const App = (params) => {
-//   function fn() {
-//     return "app"
-//   }
-//   function someFn(func) {
+//   function fn(func) {
 //     return func()
 //   }
-//   return <h1>{someFn(fn)}</h1>
-// }
 
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
-
-// ====================================================================================================================================================================
-
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-
-// const App = (params) => {
-//   function fn() {
-//     return "app"
-//   }
-//   const someFn = fn
-//   return <h1>{someFn()}</h1>
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
-
-// ====================================================================================================================================================================
-
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-
-// const App = (params) => {
-//   function fn() {
-//     return "app"
-//   }
-
-//   return <h1>{fn()}</h1>
+//   return <h1>{fn(someFn())}</h1>
 // }
 
 // const root = ReactDOM.createRoot(document.getElementById('root'))
