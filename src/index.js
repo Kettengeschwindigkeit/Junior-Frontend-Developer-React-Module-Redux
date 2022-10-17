145b47a4b5b85f45dc38bb91b4bc1de867426306
Kettengeschwindigkeit <666damned999@mail.ru>
2022-10-17 14:06:47 +0300

    Redux. Part I. Functions composition

--- src/index.js
+++ src/index.js
@@ -1,48 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { compose, pipe } from "lodash/fp"
 
 const App = (params) => {
-  const arr = ["some", "new", "data"]
+  const x = 2
+  // const y = x * 2 
+  // const z = y * y 
+  const double = (number) => number * 2
+  const square = (number) => number * number
+  const half = (number) => number / 2
+  const mathCalculate = compose(half, square, double)
+  const mathCalculate1 = pipe(double, square, half)
 
-  function formatArray(el) {return el + " some"}
-
-  setTimeout(() => {
-
-  }, timeout)
-
-  // arr.map((el) => el + " some")
-
-  return <h1>{arr.map(formatArray)}</h1>
+  return <h1>{mathCalculate(x)}, {mathCalculate1(x)}</h1>
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
 
-// ====================================================================================================================================================================
+// =====================================================================================================================================================================
 
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
+// import React from "react"
+// import ReactDOM from "react-dom/client"
 
 // const App = (params) => {
+//   const x = 2
+//   // const y = x * 2 
+//   // const z = y * y 
+//   const double = (number) => number * 2
+//   const square = (number) => number * number
+//   const half = (number) => number / 2
 
-//   function someFn() {
-//     return function() {
-//       return "app"
-//     }
-//   }
-
-//   function fn(func) {
-//     return func()
-//   }
-
-//   return <h1>{fn(someFn())}</h1>
+//   return <h1>{half(square(double(x)))}</h1>
 // }
 
-// const root = ReactDOM.createRoot(document.getElementById('root'))
+// const root = ReactDOM.createRoot(document.getElementById("root"))
 // root.render(
 //   <React.StrictMode>
 //     <App />
