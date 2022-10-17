b7e28e8e6eb1f9606ad27edde8eb152c21ac4e61
Kettengeschwindigkeit <666damned999@mail.ru>
2022-10-17 17:35:03 +0300

    Redux. Part I. Homework

--- src/store/store.js
+++ src/store/store.js
@@ -9,15 +9,3 @@ const initialState = [
 export function initiateStore() {
     return createStore(taskReducer, initialState)
 }
-
-// ==================================================================================================================================================================
-
-// import { createStore } from "redux"
-// import { taskReducer } from "./taskReducer"
-
-// const initialState = [
-//     { id: 1, title: "Task 1", completed: false },
-//     { id: 2, title: "Task 2", completed: false }
-// ]
-
-// const store = createStore(taskReducer, initialState)
