6b62489e0319ec03e05119c757d5310d3d7cca60
Kettengeschwindigkeit <666damned999@mail.ru>
2022-10-17 14:19:31 +0300

    Redux. Part I. Currying

--- src/index.js
+++ src/index.js
@@ -9,10 +9,15 @@ const App = (params) => {
   const double = (number) => number * 2
   const square = (number) => number * number
   const half = (number) => number / 2
-  const mathCalculate = compose(half, square, double)
-  const mathCalculate1 = pipe(double, square, half)
+  const divide = (num2) => {
+    return function(num1) {
+      return num1 / num2
+    }
+  }
+  
+  const mathCalculate = pipe(double, square, half, divide(3))
 
-  return <h1>{mathCalculate(x)}, {mathCalculate1(x)}</h1>
+  return <h1>{mathCalculate(x)}</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
@@ -21,6 +26,31 @@ root.render(
     <App />
   </React.StrictMode>
 )
+
+// =====================================================================================================================================================================
+
+// import React from "react"
+// import ReactDOM from "react-dom/client"
+// import { compose, pipe } from "lodash/fp"
+
+// const App = (params) => {
+//   const x = 2
+//   // const y = x * 2 
+//   // const z = y * y 
+//   const double = (number) => number * 2
+//   const square = (number) => number * number
+//   const half = (number) => number / 2
+//   const mathCalculate = compose(half, square, double)
+//   const mathCalculate1 = pipe(double, square, half)
+
+//   return <h1>{mathCalculate(x)}, {mathCalculate1(x)}</h1>
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
 
 // =====================================================================================================================================================================
 
