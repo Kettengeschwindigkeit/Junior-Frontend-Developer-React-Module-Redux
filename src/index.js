11382430d84886cfc038a00dd5b828c96fcdd39a
Kettengeschwindigkeit <666damned999@mail.ru>
2022-10-17 14:59:19 +0300

    Redux. Part I. Immutable object

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 import { compose, pipe } from "lodash/fp"
 
 const App = (params) => {
-  const x = 2
-  const y = 6
-  const double = (number) => number * 2
-  const square = (number) => number * number
-  const half = (number, y) => number / y
+  const [state, setState] = useState({})
 
-  console.log(half(12))
-  y = 2 
-  console.log(half(12))
+  // setState((prevState) => ({ ...prevState, id: "12312" }))
 
-  const divide = (num2) => {
-    return function(num1) {
-      return num1 / num2
-    }
-  }
+  const obj1 = { id: 2, name: "Some name", author: { name: "some name" } }
+  const obj2 = { ...obj1, author: { ...obj1.author } }
 
-  const mathCalculate = pipe(double, square, half, divide(3))
+  console.log(obj1.author === obj2.author)
 
-  return <h1>{mathCalculate(x)}</h1>
+  return <h1>App</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
@@ -30,104 +21,3 @@ root.render(
     <App />
   </React.StrictMode>
 )
-
-// =====================================================================================================================================================================
-
-// import React from "react"
-// import ReactDOM from "react-dom/client"
-// import { compose, pipe } from "lodash/fp"
-
-// const App = (params) => {
-//   const x = 2
-//   const y = 6
-//   const double = (number) => number * 2
-//   const square = (number) => number * number
-//   const half = (number) => number / y
-
-//   console.log(half(12))
-//   y = 2 
-//   console.log(half(12))
-
-//   const divide = (num2) => {
-//     return function(num1) {
-//       return num1 / num2
-//     }
-//   }
-
-//   const mathCalculate = pipe(double, square, half, divide(3))
-
-//   return <h1>{mathCalculate(x)}</h1>
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
-
-// =====================================================================================================================================================================
-
-// import React from "react"
-// import ReactDOM from "react-dom/client"
-// import { compose, pipe } from "lodash/fp"
-
-// const App = (params) => {
-//   const x = 2
-//   let y = 6
-//   const double = (number) => number * 2
-//   const square = (number) => number * number
-//   const half = (number) => number / y
-
-//   console.log(half(12))
-//   y = 2 
-//   console.log(half(12))
-  
-//   const divide = (num2) => {
-//     return function(num1) {
-//       return num1 / num2
-//     }
-//   }
-
-//   const mathCalculate = pipe(double, square, half, divide(3))
-
-//   return <h1>{mathCalculate(x)}</h1>
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
-
-// =====================================================================================================================================================================
-
-// import React from "react"
-// import ReactDOM from "react-dom/client"
-// import { compose, pipe } from "lodash/fp"
-
-// const App = (params) => {
-//   const x = 2
-
-//   const double = (number) => number * 2
-//   const square = (number) => number * number
-//   const half = (number) => number / Math.random()
-
-//   const divide = (num2) => {
-//     return function(num1) {
-//       return num1 / num2
-//     }
-//   }
-
-//   const mathCalculate = pipe(double, square, half, divide(3))
-
-//   return <h1>{mathCalculate(x)}</h1>
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
