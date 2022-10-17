cecd37217595a26425b63ad1358819fa46f9891f
Kettengeschwindigkeit <666damned999@mail.ru>
2022-10-17 14:49:39 +0300

    Redux. Part I. Pure functions

--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import { compose, pipe } from "lodash/fp"
 
 const App = (params) => {
   const x = 2
-  // const y = x * 2 
-  // const z = y * y 
+  const y = 6
   const double = (number) => number * 2
   const square = (number) => number * number
-  const half = (number) => number / 2
+  const half = (number, y) => number / y
+
+  console.log(half(12))
+  y = 2 
+  console.log(half(12))
+
   const divide = (num2) => {
     return function(num1) {
       return num1 / num2
     }
   }
-  
+
   const mathCalculate = pipe(double, square, half, divide(3))
 
   return <h1>{mathCalculate(x)}</h1>
@@ -35,15 +39,24 @@ root.render(
 
 // const App = (params) => {
 //   const x = 2
-//   // const y = x * 2 
-//   // const z = y * y 
+//   const y = 6
 //   const double = (number) => number * 2
 //   const square = (number) => number * number
-//   const half = (number) => number / 2
-//   const mathCalculate = compose(half, square, double)
-//   const mathCalculate1 = pipe(double, square, half)
+//   const half = (number) => number / y
 
-//   return <h1>{mathCalculate(x)}, {mathCalculate1(x)}</h1>
+//   console.log(half(12))
+//   y = 2 
+//   console.log(half(12))
+
+//   const divide = (num2) => {
+//     return function(num1) {
+//       return num1 / num2
+//     }
+//   }
+
+//   const mathCalculate = pipe(double, square, half, divide(3))
+
+//   return <h1>{mathCalculate(x)}</h1>
 // }
 
 // const root = ReactDOM.createRoot(document.getElementById("root"))
@@ -51,21 +64,65 @@ root.render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>
+// )
 
 // =====================================================================================================================================================================
 
 // import React from "react"
 // import ReactDOM from "react-dom/client"
+// import { compose, pipe } from "lodash/fp"
 
 // const App = (params) => {
 //   const x = 2
-//   // const y = x * 2 
-//   // const z = y * y 
+//   let y = 6
 //   const double = (number) => number * 2
 //   const square = (number) => number * number
-//   const half = (number) => number / 2
+//   const half = (number) => number / y
 
-//   return <h1>{half(square(double(x)))}</h1>
+//   console.log(half(12))
+//   y = 2 
+//   console.log(half(12))
+  
+//   const divide = (num2) => {
+//     return function(num1) {
+//       return num1 / num2
+//     }
+//   }
+
+//   const mathCalculate = pipe(double, square, half, divide(3))
+
+//   return <h1>{mathCalculate(x)}</h1>
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+// =====================================================================================================================================================================
+
+// import React from "react"
+// import ReactDOM from "react-dom/client"
+// import { compose, pipe } from "lodash/fp"
+
+// const App = (params) => {
+//   const x = 2
+
+//   const double = (number) => number * 2
+//   const square = (number) => number * number
+//   const half = (number) => number / Math.random()
+
+//   const divide = (num2) => {
+//     return function(num1) {
+//       return num1 / num2
+//     }
+//   }
+
+//   const mathCalculate = pipe(double, square, half, divide(3))
+
+//   return <h1>{mathCalculate(x)}</h1>
 // }
 
 // const root = ReactDOM.createRoot(document.getElementById("root"))
