057cedeb18f793c15290f7480b829fed9a3a4fdb
Kettengeschwindigkeit <666damned999@mail.ru>
2022-10-24 15:49:38 +0300

    Redux. Part II. Redux Toolkit. Create Reducer

--- src/store/task.js
+++ src/store/task.js
@@ -1,4 +1,9 @@
-import { createAction } from "@reduxjs/toolkit"
+import { createAction, createReducer } from "@reduxjs/toolkit"
+
+const initialState = [
+    { id: 1, title: "Task 1", completed: false },
+    { id: 2, title: "Task 2", completed: false }
+]
 
 const update = createAction("task/updated")
 const remove = createAction("task/removed")
@@ -15,61 +20,26 @@ export function taskDeleted(id) {
     return remove({ id })
 }
 
-function taskReducer(state = [], action) {
-    switch (action.type) {
-        case update.type: {
-            const newArray = [...state]
-            const elementIndex = newArray.findIndex((el) => el.id === action.payload.id)
-            newArray[elementIndex] = { ...newArray[elementIndex], ...action.payload }
-            return newArray
-        }
-        case remove.type: {
-            const newArray = [...state]
-            const filteredArray = newArray.filter(el => el.id !== action.payload.id)
-            return filteredArray
-        }
-        default:
-            return state
-    }
-}
-
-export default taskReducer
-
-// ================================================================================================================================================================================================
-
-// const TASK_UPDATED = "task/updated";
-// const TASK_DELETED = "task/deleted";
-
-// export function taskCompleted(id) {
-//     return {
-//         type: TASK_UPDATED,
-//         payload: { id, completed: true }
-//     }
-// }
-
-// export function titleChanged(id) {
-//     return {
-//         type: TASK_UPDATED,
-//         payload: { id, title: `New title for ${id}` }
-//     }
-// }
-
-// export function taskDeleted(id) {
-//     return {
-//         type: TASK_DELETED,
-//         payload: { id }
-//     }
-// }
+const taskReducer = createReducer(initialState, (builder) => {
+    builder
+        .addCase(update, (state, action) => {
+            const elementIndex = state.findIndex((el) => el.id === action.payload.id)
+            state[elementIndex] = { ...state[elementIndex], ...action.payload }
+        })
+        .addCase(remove, (state, action) => {
+            return state.filter(el => el.id !== action.payload.id)
+        })
+})
 
 // function taskReducer(state = [], action) {
 //     switch (action.type) {
-//         case TASK_UPDATED: {
+//         case update.type: {
 //             const newArray = [...state]
 //             const elementIndex = newArray.findIndex((el) => el.id === action.payload.id)
 //             newArray[elementIndex] = { ...newArray[elementIndex], ...action.payload }
 //             return newArray
 //         }
-//         case TASK_DELETED: {
+//         case remove.type: {
 //             const newArray = [...state]
 //             const filteredArray = newArray.filter(el => el.id !== action.payload.id)
 //             return filteredArray
@@ -79,4 +49,4 @@ export default taskReducer
 //     }
 // }
 
-// export default taskReducer
+export default taskReducer
