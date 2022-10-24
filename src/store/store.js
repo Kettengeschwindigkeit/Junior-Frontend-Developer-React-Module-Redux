295778e9607c88d7016bf22a84bca260e436cabd
Kettengeschwindigkeit <666damned999@mail.ru>
2022-10-24 16:33:56 +0300

    Redux. Part II. Creating Middleware. Logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,11 @@
-import { createStore } from "redux"
+import { createStore, compose, applyMiddleware } from "redux"
+import { logger } from "./middleware/logger"
 import taskReducer from "./task"
 
+const middlewareEnhancer = applyMiddleware(logger)
+
 function configureStore() {
-    return createStore(taskReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    return createStore(taskReducer, compose(middlewareEnhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 }
 
 export default configureStore
