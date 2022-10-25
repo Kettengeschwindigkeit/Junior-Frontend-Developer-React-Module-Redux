de85d164efba3c6607b7afdce4fa8f97ff3bc020
Kettengeschwindigkeit <666damned999@mail.ru>
2022-10-25 11:56:40 +0300

    Redux. Part II. React Redux. useDispatch()

--- src/index.js
+++ src/index.js
@@ -2,23 +2,24 @@ import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom/client"
 import { titleChanged, taskDeleted, completeTask, getTasks } from "./store/task"
 import configureStore from "./store/store"
-import { Provider, useSelector } from "react-redux"
+import { Provider, useDispatch, useSelector } from "react-redux"
 
 const store = configureStore()
 
 const App = (params) => {
   const state = useSelector((state) => state)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    store.dispatch(getTasks())
+    dispatch(getTasks())
   }, [])
 
   const changeTitle = (taskId) => {
-    store.dispatch(titleChanged(taskId))
+    dispatch(titleChanged(taskId))
   }
 
   const deleteTask = (taskId) => {
-    store.dispatch(taskDeleted(taskId))
+    dispatch(taskDeleted(taskId))
   }
 
   return (
@@ -29,7 +30,7 @@ const App = (params) => {
           <li key={el.id}>
             <p>{el.title}</p>
             <p>{`Completed: ${el.completed}`}</p>
-            <button onClick={() => store.dispatch(completeTask(el.id))}>Complete</button>
+            <button onClick={() => dispatch(completeTask(el.id))}>Complete</button>
             <button onClick={() => changeTitle(el.id)}>Change title</button>
             <button onClick={() => deleteTask(el.id)}>Delete task</button>
             <hr />
