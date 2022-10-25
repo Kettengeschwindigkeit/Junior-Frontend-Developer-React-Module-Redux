e7fca0a070cdc676bd4fbe3f1026825bacb18adf
Kettengeschwindigkeit <666damned999@mail.ru>
2022-10-25 11:46:43 +0300

    Redux. Part II. React Redux. Providing Store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom/client"
 import { titleChanged, taskDeleted, completeTask, getTasks } from "./store/task"
 import configureStore from "./store/store"
+import { Provider } from "react-redux"
 
 const store = configureStore()
 
@@ -45,6 +46,8 @@ const App = (params) => {
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
